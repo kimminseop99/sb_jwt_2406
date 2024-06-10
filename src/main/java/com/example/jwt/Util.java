01d83ebff5abb2209b54778e89b5311315859264
kimminseop99 <minseopkim99@gmail.com>
2024-06-10 16:52:51 +0900

    access Token, claims, verify

--- src/main/java/com/example/jwt/Util.java
+++ src/main/java/com/example/jwt/Util.java
@@ -1,9 +1,9 @@
 package com.example.jwt;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Util {
@@ -18,4 +18,11 @@ public class Util {
         }
     }
 
+    public static Map<String, Object> toMap(String jsonStr) {
+        try {
+            return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
 }

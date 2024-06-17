1739697a42aa0a60d127a23ba7a1c099929e2604
kimminseop99 <minseopkim99@gmail.com>
2024-06-17 14:44:59 +0900

    add member/me

--- src/main/java/com/example/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jwt/domain/member/controller/MemberController.java
@@ -10,11 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.util.MimeTypeUtils.ALL_VALUE;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -38,7 +36,7 @@ public class MemberController {
         private final  String accessToken;
     }
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", consumes = APPLICATION_JSON_VALUE)
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
 
         // 테스트용
@@ -52,6 +50,23 @@ public class MemberController {
                 "S-1",
                 "액세스 토큰이 생성되었습니다.",
                 new LoginResponse(accessToken)
+        );
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class MeResponse {
+        private final Member member;
+    }
+
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me () {
+        Member member = memberService.findByUsername("user1").get();
+
+        return RsData.of(
+                "S-2",
+                "성공",
+                new MeResponse(member)
         );
     }
 }

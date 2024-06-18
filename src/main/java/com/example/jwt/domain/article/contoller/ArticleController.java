10223eed2f2e7f5ce09bcd9d525a13a0fc843875
kimminseop99 <minseopkim99@gmail.com>
2024-06-18 14:40:43 +0900

    SpringDocConfig, add articles

--- src/main/java/com/example/jwt/domain/article/contoller/ArticleController.java
+++ src/main/java/com/example/jwt/domain/article/contoller/ArticleController.java
@@ -1,15 +1,40 @@
 package com.example.jwt.domain.article.contoller;
 
+import com.example.jwt.domain.article.entity.Article;
+import com.example.jwt.domain.article.service.ArticleService;
+import com.example.jwt.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/v1/article", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/articles")
 public class ArticleController {
+    private final ArticleService articleService;
 
+    @AllArgsConstructor
+    @Getter
+    public static class ArticleResponse{
+        private final List<Article> articles;
+
+    }
+
+    @GetMapping(value = "")
+    @Operation(summary = "게시물들")
+    public RsData<ArticleResponse> article(){
+        List<Article> articles = articleService.findAll();
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new ArticleResponse(articles)
+        );
+    }
 }
--- src/main/java/com/example/jwt/global/security/SecurityConfig.java
+++ src/main/java/com/example/jwt/global/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
                                 .requestMatchers("/api/*/member/login").permitAll() // 로그인은 누구나 가능
+                                .requestMatchers("/api/*/articles").permitAll() // 글 보기는 누구나 가능
                                 .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
                 )
                 .cors(
@@ -35,7 +36,7 @@ public class SecurityConfig {
                 ) // CSRF 토큰 끄기
                 .httpBasic(
                         httpBasic -> httpBasic.disable()
-                ) // httpBaic 로그인 방식 끄기
+                ) // httpBasic 로그인 방식 끄기
                 .formLogin(
                         formLogin -> formLogin.disable()
                 ) // 폼 로그인 방식 끄기

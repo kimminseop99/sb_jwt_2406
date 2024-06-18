5ba2a43f4bfb322fa03ace7915c1717207c9eb21
kimminseop99 <minseopkim99@gmail.com>
2024-06-18 15:14:08 +0900

    articles row

--- src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class ArticleController {
 
     @AllArgsConstructor
     @Getter
-    public static class ArticleResponse{
+    public static class ArticlesResponse{
         private final List<Article> articles;
 
     }
@@ -36,5 +37,26 @@ public class ArticleController {
                 "성공",
                 new ArticleResponse(articles)
         );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ArticleResponse{
+        private final Article article;
+
+    }
+
+    @GetMapping(value = "/{id}")
+    @Operation(summary = "단건조회")
+    public RsData<ArticleResponse> article(@PathVariable("id") Long id){
+        return articleService.findById(id).map(article -> RsData.of(
+                "S-1",
+                "성공",
+                new ArticleResponse(article)
+        )).orElseGet(() -> RsData.of(
+                "F-1",
+                "%d번 게시물은 존재하지 않습니다.".formatted(id),
+                null
+        ));
     }
 }
